fd6e059f496325679cd2e270e845f3cb06fffd77
MATTIA BORRONI <153180606+mattiaborroni@users.noreply.github.com>
2024-06-16 22:52:24 +0200

    Update run.sh

--- nx584/run.sh
+++ nx584/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/with-contenv bashio
-python3 /usr/bin/nx584_server --serial /dev/ttyUSB0 --baud 9600 --config /usr/app/src/config.ini
+python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --serial /dev/ttyUSB0 --baud 9600 --config /usr/app/src/config.ini
